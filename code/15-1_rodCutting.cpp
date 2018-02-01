fe79df4fce787d1674bfa8d1991ef3397adeefa5
pouder-Man <vkdnej91@gmail.com>
2018-02-01 20:08:02 +0900

    add rodCutting alg

---
+++ code/15-1_rodCutting.cpp
@@ -0,0 +1,33 @@
+//
+// Created by vkdne on 2018-02-01.
+//
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int dp[1001];
+
+int main(){
+
+    int N;
+    int arr[1001];
+
+    cin >> N;
+    for(int i = 1; i <= N ; i++){
+        cin >> arr[i];
+    }
+
+    dp[1] = arr[1];
+
+    for(int i = 2; i <= N ; i++){
+        int temp = arr[i];
+        for(int j = 1; j < N ; j++){
+            if(temp < dp[i-j] + dp[j])
+                temp = dp[i-j] + dp[j];
+        }
+        dp[i] = temp;
+        cout<<"dp["<<i<<"] = "<<dp[i]<<endl;
+    }
+    cout << dp[N];
+    return 0;
+}
